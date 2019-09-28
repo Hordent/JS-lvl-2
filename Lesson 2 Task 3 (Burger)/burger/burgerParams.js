390bec0a56441fe0dbc163c836acaae233db8df6
Hordent <hordydmitry@gmail.com>
2019-09-28 22:48:07 +0500

    task 3 Burger

---
+++ Lesson 2 Task 3 (Burger)/burger/burgerParams.js
@@ -0,0 +1,54 @@
+'use strict';
+const burgerParams = {
+    size: [
+        {
+            id: 1,
+            title: 'Маленький',
+            price: 50,
+            calories: 20
+        },
+        {
+            id: 2,
+            title: 'Большой',
+            price: 100,
+            calories: 40
+        }
+    ],
+
+    stuffing: [
+        {
+            id: 1,
+            title: 'С сыром',
+            price: 10,
+            calories: 20
+        },
+        {
+            id: 2,
+            title: 'С салатом',
+            price: 20,
+            calories: 5
+        },
+        {
+            id: 3,
+            title: 'С картофелем',
+            price: 15,
+            calories: 10
+        }
+    ],
+
+    toppings: [
+        {
+            id: 1,
+            title: 'Посыпать приправой',
+            price: 15,
+            calories: 0
+        },
+        {
+            id: 2,
+            title: 'Полить майонезом',
+            price: 20,
+            calories: 5
+        },
+    ]
+
+}
